18f9b6c720e48548022cbe52781fddb7d3f1ca59
Bob Westbroek <bob.westbroek@aggiemail.usu.edu>
2022-03-08 10:37:33 -0700

    removed network interface

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,9 @@
-resource "aws_network_interface" "my_interface" {
-    subnet_id   = aws_subnet.my_subnet.id
-    private_ip  = "192.168.0.100"
-}
-
 resource "aws_instance" "server" {
     ami                         = var.image
     associate_public_ip_address = "true"
     instance_type               = "t2.micro"
     availability_zone           = var.az
     get_password_data           = "true"
-    network_interface {
-      network_interface_id      = aws_network_interface.my_interface.id
-      device_index              = 0
-    }
     root_block_device {
       volume_size = "30"
     }
