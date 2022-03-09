665a5d91b288d6bdd7f8ece8505dc3f244e41b13
Bob Westbroek <bob.westbroek@aggiemail.usu.edu>
2022-03-09 12:11:25 -0700

    commented out security group

--- ec2.tf
+++ ec2.tf
@@ -6,19 +6,19 @@ resource "aws_instance" "server" {
     root_block_device {
       volume_size = "30"
     }
-    key_name = "terraform-key"
+    #key_name = "terraform-key"
     security_groups = [aws_security_group.allow_rdp.name]
 }
 
-resource "aws_security_group" "allow_rdp" {
-  name = "allow_rdp"
-  description = "Allow rdp traffic"
+#resource "aws_security_group" "allow_rdp" {
+ # name = "allow_rdp"
+  #description = "Allow rdp traffic"
 
-  ingress {
-    from_port = 3389
-    to_port = 3389
-    protocol = "tcp"
+  #ingress {
+   # from_port = 3389
+    #to_port = 3389
+    #protocol = "tcp"
 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+    #cidr_blocks = ["0.0.0.0/0"]
+  #}
+#}
