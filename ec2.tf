1f5277480619270edb17b76aa269e337321b65e5
Bob Westbroek <bob.westbroek@aggiemail.usu.edu>
2022-03-09 12:19:05 -0700

    used block commenting

--- ec2.tf
+++ ec2.tf
@@ -10,15 +10,15 @@ resource "aws_instance" "server" {
     #security_groups = [aws_security_group.allow_rdp.name]
 }
 
-#resource "aws_security_group" "allow_rdp" {
- # name = "allow_rdp"
-  #description = "Allow rdp traffic"
+/* resource "aws_security_group" "allow_rdp" {
+ name = "allow_rdp"
+ description = "Allow rdp traffic"
 
-  #ingress {
-   # from_port = 3389
-    #to_port = 3389
-    #protocol = "tcp"
+  ingress {
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
 
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
-#}
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} */
