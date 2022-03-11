f78b0025d34163ed8bb94bacc4da92e306d6f4d1
Bob Westbroek <bob.westbroek@aggiemail.usu.edu>
2022-03-11 08:01:10 -0700

    added egress rule

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "server" {
+    name                        = "terraform practice"
     ami                         = var.image
     associate_public_ip_address = "true"
     instance_type               = "t2.micro"
@@ -45,6 +46,19 @@ resource "aws_security_group" "allow_https" {
     to_port = 443
     protocol = "tcp"
 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_all_out" {
+  name = "allow all out"
+  description = "Allow all outbound traffic"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
