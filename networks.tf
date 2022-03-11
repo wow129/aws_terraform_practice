bd14ef6f7ff25886c032af8841279ee0f1c3fc82
Bob Westbroek <bob.westbroek@aggiemail.usu.edu>
2022-03-11 15:17:39 -0700

    added route table

--- networks.tf
+++ networks.tf
@@ -20,3 +20,12 @@ resource "aws_internet_gateway" "terraform" {
         Name = "Terraform Gateway"
     }
 }
+
+resource "aws_route_table" "terraform" {
+    vpc_id = aws_vpc.terraform.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.terraform.id
+    }
+}
